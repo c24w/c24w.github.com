c65b68024fd4d0dcd1949bdbe72071f467ab596e
Chris Watson <chris24watson@gmail.com>
2012-12-16 22:30:10 +0000

    Refactor

--- fiddles/parallax-scrolling/script.js
+++ fiddles/parallax-scrolling/script.js
@@ -1,68 +1,93 @@
-﻿var b = document.body;
-var plane = document.getElementById('plane');
-var clouds = [];
-var index = 0;
-var cw = b.clientWidth; // page width
-var ch = b.clientHeight; // page height
-var vw = window.innerWidth; // viewport width
-var vh = window.innerHeight; // viewport height
+﻿window.addEventListener('DOMContentLoaded', function () {
+	var b = document.body;
+	b.onmousedown = function (e) { e.preventDefault() };
+	var pageWidth = function () { return b.offsetWidth };
+	var pageHeight = function () { return b.offsetHeight };
+	var viewWidth = window.innerWidth;
+	var viewHeight = window.innerHeight;
+	var box = document.getElementById('cropArea');
+	var plane = document.getElementById('plane');
+	var noClouds = Math.floor(pageWidth() * pageHeight() / 40000);
+	var clouds = [];
 
-/* added 'box' to crop clouds moving beyond defined dimensions, rather than allowing the 'body' to grow indefinitely (becomes glitchy) */
-var box = document.getElementById('cropArea');
-box.style.width = cw + 'px';
-box.style.height = ch + 'px';
+	function Cloud(w, h, x, y, layer) {
+		this.element = document.createElement('img');
+		this.element.src = 'cloud.png';
+		this.element.className = 'cloud ' + layer;
+		var style = this.style = this.element.style;
+		style.width = w + 'px';
+		style.height = h + 'px';
+		this.startX = x;
+		this.startY = y;
+		this.position = function (x, y) {
+			style.left = x + 'px';
+			style.top = y + 'px';
+		}
+		this.position(x, y);
+	}
 
-// add random clouds
-var noClouds = cw * ch / 100000;
-while(noClouds > 0){
-	var w = 254;
-	var h = 198;
-	var e = document.createElement('img');
-	e.src = 'http://i49.tinypic.com/552pah.png';
-	e.className = 'cloud';
-	var x = Math.floor(Math.random() * (cw - w));
-	var y = Math.floor(Math.random() * (ch - h));
-	e.style.left = x + 'px';
-	e.style.top = y + 'px';
-	if(noClouds % 3 === 0){ // randomly bigger cloud at interval
+	b.onload = function () {
+		box.style.width = pageWidth() + 'px';
+		box.style.height = pageHeight() + 'px';
+
+		for (var i = 0; i < noClouds; i++) {
+			var w = scaleRand(254);
+			var h = scaleRand(198);
+			var x = randInt(pageWidth() - w);
+			var y = randInt(pageHeight() - h);
+			var layer = (i % 2 === 0) ? 'background' : 'foreground';
+			var cloud = new Cloud(w, h, x, y, layer);
+			if (i % 2 == 0) clouds[i] = cloud;
+			box.appendChild(cloud.element);
+		}
+	};
+
+	function scaleRand(baseDimension) {
 		var scalar = 1 + Math.random();
-		w = Math.floor(w * scalar);
-		h = Math.floor(h * scalar);
-		e.style.width = w + 'px';
-		e.style.height = h + 'px';
-		e.style.zIndex = '0';
-		e.setAttribute('data-xy', x + ',' + y);
-		clouds[index] = e;
-		index++;
+		return Math.floor(baseDimension * scalar);
 	}
-	box.appendChild(e);
-	noClouds--;
-}
 
-window.onscroll = scrollElements;
-};
-
-function scrollElements(){
-	var bgAmount = 0.9;
-	var cloudAmount = 0.5;
-	var x,y;
-	// clouds
-	for(var i = 0; i < clouds.length; i++){
-		var c = clouds[i];
-		var xy = c.getAttribute('data-xy').split(',');
-		x =  (b.scrollLeft * cloudAmount) + parseInt(xy[0]);
-		y =  (b.scrollTop * cloudAmount) + parseInt(xy[1]);
-		//      if(x > cw - c.clientWidth)
-		//        x = cw - c.clientWidth;
-		c.style.left = x + 'px';
-		c.style.top = y + 'px';
+	function randInt(x) {
+		return Math.floor(Math.random() * x);
 	}
-	// background
-	b.style.backgroundPosition = (b.scrollLeft * bgAmount) + 'px '
-                               + (b.scrollTop * bgAmount) + 'px';
-	// plane
-	x = (b.scrollLeft / (cw - vw)) * (vw - plane.clientWidth);
-	y = (b.scrollTop / (ch - vh)) * (vh - plane.clientHeight);
-	plane.style.left = x + 'px';
-	plane.style.top = y + 'px';
-}​
+
+	window.onscroll = function () {
+		positionBackground(0.9);
+		positionClouds(0.5);
+		positionPlane();
+	}
+
+	function positionBackground(scrollSpeed) {
+		b.style.backgroundPosition = (b.scrollLeft * scrollSpeed) + 'px ' + (b.scrollTop * scrollSpeed) + 'px';
+	}
+
+	function positionClouds(scrollSpeed) {
+		for (var i = 0; i < clouds.length; i += 2) {
+			var cloud = clouds[i];
+			var x = cloud.startX + (b.scrollLeft * scrollSpeed);
+			var y = cloud.startY + (b.scrollTop * scrollSpeed);
+			cloud.style.left = x + 'px';
+			cloud.style.top = y + 'px';
+		}
+	}
+
+	/*  +--------page--------+
+	    |   |  □   |         |
+	    |   |      |         |
+	    |   | view |         |
+	    +--------------------+  */
+
+	function positionPlane() { // location of view relative to page -> location of plane relative to view
+		var navigablePageWidth = pageWidth() - viewWidth;
+		var navigablePageHeight = pageHeight() - viewHeight;
+		var navigableViewWidth = viewWidth - plane.clientWidth;
+		var navigableViewHeight = viewHeight - plane.clientHeight;
+		var viewFromLeft = b.scrollLeft / navigablePageWidth;
+		var viewFromTop = b.scrollTop / navigablePageHeight;
+		var x = navigableViewWidth * viewFromLeft;
+		var y = navigableViewHeight * viewFromTop;
+		plane.style.left = x + 'px';
+		plane.style.top = y + 'px';
+	}
+
+});
