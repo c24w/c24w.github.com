df39eb1ef0eb6a5d7aefc2d1d8132688b0579465
Chris Watson <chris24watson@gmail.com>
2012-12-24 15:39:04 +0000

    Load stylesheet ahead of script

--- fiddles/parallax-scrolling/script.js
+++ fiddles/parallax-scrolling/script.js
@@ -37,8 +37,7 @@
 
 		var pageWidth = b.offsetWidth;
 		var pageHeight = b.offsetHeight;
-		var viewWidth = window.innerWidth;
-		var viewHeight = window.innerHeight;
+		var viewWidth = window.innerWidth;wi
 
 		var box = document.getElementById('cropArea');
 		box.style.width = pageWidth + 'px';
