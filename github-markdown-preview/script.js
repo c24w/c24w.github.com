cbc0fc30bccce3a107cda0a9721d271bed2f57ad
Chris Watson <chris.watson@7digital.com>
2013-03-05 16:26:57 +0000

    Add textarea input and hacky re-addition of line breaks

--- github-markdown-preview/script.js
+++ github-markdown-preview/script.js
@@ -1,22 +1,29 @@
 $(function executeScript() {
 
-	var markdown = prompt('Data:');
-
-	$.ajax({
-		type: "POST",
-		beforeSend: beforeSend,
-		url: 'https://api.github.com/markdown/raw',
-		data: markdown,
-		success: success
-	});
-
 	function beforeSend(request) {
 		request.setRequestHeader('Content-Type', 'text/x-markdown');
 	}
 
 	function success(data) {
+		data = data.replace(/\%0A/g,'<br>');
+		data = decodeURIComponent(data);
 		preview = $('#preview');
 		preview.html(data);
 	}
+
+	function getMarkdown() {
+		var markdown = $('#markdown')[0].value;
+		if (markdown) {
+			$.ajax({
+				type: "POST",
+				beforeSend: beforeSend,
+				url: 'https://api.github.com/markdown/raw',
+				data: encodeURIComponent(markdown),
+				success: success
+			});
+		}
+	}
+
+	$('#previewBtn').click(getMarkdown);
 
 });
