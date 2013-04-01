fb5166c41b777483cc171fd3aa2f8689b9497e81
Chris Watson <chris24watson@gmail.com>
2013-04-01 21:54:38 +0100

    Remove blog contents

--- blog/disqus.js
+++
@@ -1,9 +0,0 @@
-var disqus_shortname = 'c24w-blog',
-	disqus_identifier = window.location.pathname,
-	disqus_url = window.location.host + window.location.pathname;
-
-(function() {
-	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
